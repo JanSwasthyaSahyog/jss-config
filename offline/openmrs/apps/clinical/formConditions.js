56eb8ba9a361da99f30ba8e67b03c2d844f27c2d
Charles John <charlesj@gmail.com>
2018-01-18 17:29:02 +0530

    Adding new 'offline' folder required for bahmni-connect

---
+++ offline/openmrs/apps/clinical/formConditions.js
@@ -0,0 +1,30 @@
+Bahmni.ConceptSet.FormConditions.rules = {
+/*
+    'Diastolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    },
+    'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    }
+*/
+};
